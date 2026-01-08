d8c26b16c517bba51b6d4667faab729fb2a127a3
Vitor Fidelis <vitor.fidelis5587@gmail.com>
2026-01-07 21:47:46 -0300

    aplicando alterações na aplicação de notificação

--- lambda/src/main/java/br/com/lambda/dtos/FeedbeckMessageDto.java
+++ lambda/src/main/java/br/com/lambda/dtos/FeedbeckMessageDto.java
@@ -1,12 +1,13 @@
 package br.com.lambda.dtos;
 
 import java.time.LocalDateTime;
+import java.time.Instant;
 
 public record FeedbeckMessageDto(
         String descricao,
         Double nota,
         String urgencia,
-        LocalDateTime date
+        Instant dateEnvio
 ) {
 
 }
